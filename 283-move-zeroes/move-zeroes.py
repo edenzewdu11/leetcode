42af79ced4473301226fdf128ab5d40b2e660fe6
edenzewdu11 <167537470+edenzewdu11@users.noreply.github.com>
2024-11-13 18:32:12 +0300

    Time: 8 ms (52.96%) | Memory: 12.8 MB (47.64%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -1,16 +1,11 @@
 class Solution(object):
     def moveZeroes(self, nums):
-        
-        non_zero_elements = [num for num in nums if num != 0]
-        
-        
-        zero_count = len(nums) - len(non_zero_elements)
-        
-       
-        result = non_zero_elements + [0] * zero_count
-        
-       
-        for i in range(len(nums)):
-            nums[i] = result[i]
-        
+        left = 0
+        for right in range(len(nums)):
+            if nums[right] != 0:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
         return nums
+
+       
+        
